97ef149b7772f9fc650e94330699c4aa49cc54f5
Mahmudul-hasan0088 <try.mahmudulhasan@gmail.com>
2024-12-31 13:54:57 +0000

    Nnn

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -42,7 +42,7 @@ export default function Contact() {
         Have a question or feedback? We'd love to hear from you. Fill out the form below and we'll get back to you as soon as possible.
       </p>
       <form onSubmit={handleSubmit} className="space-y-6">
-        <input type="hidden" name="access_key" value="YOUR_WEB3FORMS_ACCESS_KEY" />
+        <input type="hidden" name="access_key" value="ab930cd8-4594-4c75-ba38-3a231f9ae1ea" />
         <div>
           <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
           <Input type="text" id="name" name="name" required className="mt-1" />
